e24098fe9d96b66fb0c2933d6fade9d5fe1edc5c
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-29 22:57:20 -0300

    Update Questao_06.py

--- Questao_06.py
+++ Questao_06.py
@@ -1,26 +1,20 @@
 def mini_calculadora(N, dividendo, divisor):
-    melhor_solução = ()
-    if N >= dividendo and N >= divisor:
-        melhor_solução = (dividendo,divisor)
-    if N < dividendo or N < divisor:
-        x = 1
-        while dividendo > N:
-            if dividendo % 2 == 0 and divisor % 2 == 0:
-                dividendo = dividendo / 2
-                divisor = divisor / 2
-            elif dividendo % 3 == 0 and divisor % 3 == 0:
-                dividendo = dividendo / 3
-                divisor = divisor / 3
-            elif dividendo % 5 == 0 and divisor % 5 == 0:
-                dividendo = dividendo / 5
-                divisor = divisor / 5
-            elif dividendo % 7 == 0 and divisor % 7 == 0:
-                dividendo = dividendo / 7
-                divisor = divisor / 7
-            else:
-                break
-        melhor_solução = (dividendo,divisor)
-        if dividendo > N:
-            melhor_solução = 'IMPOSSIVEL'
-    return melhor_solução
+    bits = 2 ** N
+    if bits >= dividendo:
+        a = dividendo
+        b = divisor
+        # MDC = Menor divisor comum
+        while b != 0:
+            resto = a % b
+            a = b
+            b = resto
+        mdc = a
+        print(mdc)
+        x = int(dividendo / mdc)
+        y = int(divisor / mdc)
+        t = (x, y)
+        return t
+    else:
+        return 'IMPOSSÍVEL'
+
 print(mini_calculadora(30,200,90))
