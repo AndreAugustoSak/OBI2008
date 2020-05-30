9bd34c29d7e13e16ed555dfc48ce45ba176c5e98
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-29 22:57:15 -0300

    Update Questao_05.py

--- Questao_05.py
+++ Questao_05.py
@@ -1,12 +1,11 @@
 def acoes_bolsa(lista):
     p1 = 0
     p2 = 4
-    newsoma = sum(lista[p1:p2])
+    soma = []
     for c in range(len(lista)-3):
-        soma = sum(lista[p1:p2])
+        soma.append(sum(lista[p1:p2]))
         p1 += 1
         p2 += 1
-        if soma > newsoma:
-            newsoma = soma
-    return newsoma
-print(acoes_bolsa([-1,-2,-3,-4,-5,-6,-7,-8]))
+    lucro = max(soma)
+    return lucro
+print(acoes_bolsa([-2,1,-2,1,0,-3]))
