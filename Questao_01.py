e282d03dfb66e2dd9032d768e00863ef4616bc1b
Andre A. Sak <63429063+AndreAugustoSak@users.noreply.github.com>
2020-05-29 22:56:50 -0300

    Update Questao_01.py

--- Questao_01.py
+++ Questao_01.py
@@ -1,13 +1,10 @@
 def qtd_competidores(N, P, lista):
     aceitos = 0
-    for c in range(N):
-        competidor = lista[c]
-        fase1 = competidor[0]
-        fase2 = competidor[1]
-        total = fase1 + fase2
-        if total >= P:
+    for item in lista:
+        a = (sum(item))
+        if a >= P:
             aceitos += 1
     return aceitos
 
-print(qtd_competidores(3, 100, [(50,50),(100,0),(49,50)]))
+print(qtd_competidores (4, 235, [(100,134),(0,0),(200,200),(150,150)]))
 
